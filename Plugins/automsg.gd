adc2ad31f76b0d421646a8cb2396fa8d9e6cc183
IamKikin <85178201+iamkikin-off@users.noreply.github.com>
2025-07-13 05:27:05 +0200

    Add files via upload

---
+++ Plugins/automsg.gd
@@ -0,0 +1,65 @@
+extends Node
+
+onready var KikinHostBuddy = get_node_or_null("/root/KikinHostBuddy")
+
+const CONFIG_AUTOMSG_PATH = "user://automsg_config.json"
+
+# Socks API
+var Players
+var Chat
+
+var time_config = 0
+var messages_config = []
+
+var time = 0
+
+func make_example_config(file, type):
+	match type:
+		"AUTOMSG":
+			var default_data = {
+				"time": 5,
+				"messages": []
+			}
+			var err = file.open(PLUGINS_ENABLED_PATH, File.WRITE)
+			if err != OK:
+				return false
+			file.store_string(JSON.print(default_data, "\t"))
+			file.close()
+
+func load_config():
+	# Reading the config & setting variables.
+	var file = File.new()
+	if not file.file_exists(CONFIG_AUTOMSG_PATH):
+		make_example_config(file, "AUTOMSG")
+
+	var error = file.open(CONFIG_AUTOMSG_PATH, File.READ)
+	if error != OK:
+		return false
+	var content = file.get_as_text()
+	file.close()
+
+	var data = JSON.parse(content)
+	if data.error != OK:
+		return false
+	var json_data = data.result
+	
+	time_config = json_data.get("time", 0)
+	messages_config = json_data.get("messages", [])
+
+	return true
+
+func _ready():
+	Players = get_node_or_null("/root/ToesSocks/Players")
+	Chat = get_node_or_null("/root/ToesSocks/Chat")
+	load_config()
+
+func _process(delta):
+	if Players:
+		if Players.in_game:
+			time = time + delta
+			if time >= time_config:
+				var random_msg = messages_config
+				random_msg.shuffle()
+				var final_msg = random_msg[0]
+				Chat.send_raw(final_msg, "gray", false)
+				time = 0
---
+++ Plugins/goodbyemsg.gd
@@ -0,0 +1,56 @@
+# -----------------------------------------------------------------------
+extends Node
+# -----------------------------------------------------------------------
+onready var KikinHostBuddy = get_node_or_null("/root/KikinHostBuddy")
+
+const CONFIG_GOODBYEMSG_PATH = "user://goodbyemsg_config.json"
+
+# Socks API
+var Players
+var Chat
+
+var config_goodbye_msg
+# -----------------------------------------------------------------------
+func make_example_config(file, type):
+	match type:
+		"GOODBYE_MSG":
+			var default_data = {
+				"goodbye_msg": ""
+			}
+			var err = file.open(PLUGINS_ENABLED_PATH, File.WRITE)
+			if err != OK:
+				return false
+			file.store_string(JSON.print(default_data, "\t"))
+			file.close()
+# -----------------------------------------------------------------------
+func load_config():
+	# Reading the config & setting variables.
+	var file = File.new()
+	if not file.file_exists(CONFIG_GOODBYEMSG_PATH):
+		make_example_config(file, "GOODBYE_MSG")
+
+	var error = file.open(CONFIG_GOODBYEMSG_PATH, File.READ)
+	if error != OK:
+		return false
+	var content = file.get_as_text()
+	file.close()
+
+	var data = JSON.parse(content)
+	if data.error != OK:
+		return false
+	var json_data = data.result
+	
+	config_goodbye_msg = json_data.get("goodbye_msg", "")
+
+	return true
+# -----------------------------------------------------------------------
+func _ready():
+	Players = get_node_or_null("/root/ToesSocks/Players")
+	Chat = get_node_or_null("/root/ToesSocks/Chat")
+	Players.connect("player_removed", self, "_player_removed")
+	load_config()
+# -----------------------------------------------------------------------
+func _player_removed(player):
+	config_goodbye_msg = config_goodbye_msg.replace("$PLAYER", Players.get_username(player))
+	Chat.send_raw(config_goodbye_msg, "grey", false)
+# -----------------------------------------------------------------------
